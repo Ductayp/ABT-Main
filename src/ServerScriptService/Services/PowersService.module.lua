957dd9719926ea3926c702090f247ecd54a82287
Ductape <verdantis@gmail.com>
2020-11-02 08:49:39 -0800

    Update PowersService.module.lua

--- src/ServerScriptService/Services/PowersService.module.lua
+++ src/ServerScriptService/Services/PowersService.module.lua
@@ -1,5 +1,11 @@
 -- Powers Service
+-- PDab
+-- 11/2/2020
 
+-- setup Knit
 local Knit = require(game:GetService("ReplicatedStorage").Knit)
-
 local PowersService = Knit.CreateService { Name = "PowersService", Client = {}}
+
+function PowersService:KNitIniti()
+    
+end

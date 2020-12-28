c9e55969cdd2aeb3d0e857605345c3e8cbc8ef60
Ductayp <38441484+Ductayp@users.noreply.github.com>
2020-12-27 22:51:48 -0800

    beep

--- src/ServerScriptService/Services/GamePassService.lua
+++ src/ServerScriptService/Services/GamePassService.lua
@@ -6,6 +6,8 @@
 local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
+local MarketplaceService = game:GetService("MarketplaceService")
+
 
 -- setup Knit
 local Knit = require(ReplicatedStorage:FindFirstChild("Knit",true))
@@ -14,11 +16,22 @@ local GamePassService = Knit.CreateService { Name = "GamePassService", Client = 
 -- modules
 local utils = require(Knit.Shared.Utils)
 
+local gamePasses = {
+    MobileStandStorage = 13434519,
+    DoubleCash = 13434733,
+    DoubleArrowLuck = 13434798,
+    ItemFinder = 13434805
+}
 
 --// PlayerAdded
 function GamePassService:PlayerAdded(player)
 
-
+    -- get gamepasses for player and create states for them
+    for passName,passId in pairs(gamePasses) do
+        if MarketplaceService:UserOwnsGamePassAsync(player.UserId,passId) then
+            Knit.Services.StateService:AddEntryToState(player, "GamePass", passName, true)
+        end
+    end
 end
 
 --// PlayerRemoved
